c10285b3cb2df54702e0f4650ef0067bdbaa06ac
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-09-02 15:49:30 +0100

    feat: updated composer details this was to show Enwongo how to use git

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use Illuminate\Http\Request;
+use Jenssegers\Agent\Agent;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    // $visitor = Tracker::currentSession();
+    $agent = new Agent();
+    
+    // Get device type (phone, tablet, desktop, etc.)
+    $deviceType = $agent->device();
+
+    // Get platform (Operating System)
+    $platform = $agent->platform();
+
+    // Get browser name
+    $browser = $agent->browser();
+
+    return response()->json([
+        'device' => $deviceType,
+        'platform' => $platform,
+        'browser' => $browser,
+    ]);
+
+    // dd(Request->userAgent());
+
+    // return request()->userAgent();
+
+    // return $request->userAgent();
 });
